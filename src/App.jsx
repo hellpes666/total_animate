64fbc2fbc001bbb20292433c32fe5c5af63c31e7
Barmatov Alexey <alexrui_hpow@mail.ru>
2025-01-08 19:38:23 +0300

    add features

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { Hero, About, Navbar } from './components';
+import { Hero, About, Navbar, Features } from './components';
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden ">
       <Navbar />
       <Hero />
-      
+
       <About />
+      <Features />
     </main>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ import Button from "./Button";
 import About from './About';
 import AnimatedTitle from "./AnimatedTitle";
 import Navbar from "./Navbar";
-export {Hero, Button, About, AnimatedTitle, Navbar}
+import Features from "./Features";
+export {Hero, Button, About, AnimatedTitle, Navbar, Features}
